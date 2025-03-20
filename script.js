9929c4c997bb9f76b3c6c56a33284c51d8bdbc28
Opilog <128095264+opilog12@users.noreply.github.com>
2025-03-20 16:44:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function addComment() {
+    let comment = document.getElementById("commentBox").value;
+    if (comment) {
+        let comments = document.getElementById("comments");
+        let newComment = document.createElement("p");
+        newComment.textContent = comment;
+        comments.appendChild(newComment);
+        document.getElementById("commentBox").value = "";
+    }
+}
